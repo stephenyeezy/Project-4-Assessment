68a046a07a42d78d7453848e7b1041c9105f362d
stephen yi <stephenyi@stephens-MacBook-Pro.local>
2020-10-16 16:07:46 -0400

    initial

---
+++ src/App/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import './App.css';
+import CircleSelector from '../components/CircleSelector/CircleSelector';
+import Circles from '../components/Circles/Circles';
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state={
+      selected: 'circleOne'
+    }
+  }
+
+handleSelect = (circle) => {
+  this.setState({
+    selected: circle
+  })
+}  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">UNIT 4 FINAL ASSESSMENT</header>
+        <main>
+          <CircleSelector
+            selected={this.state.selected}
+            handleSelect={this.handleSelect}
+          />
+          <Circles
+            selected={this.state.selected}
+          />
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
+
---
+++ src/components/CircleSelector/CircleSelector.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './CircleSelector.css';
+
+const CircleSelector = props => {
+  const handleChange = event => {
+      props.handleSelect(event.target.value)
+    }
+  if (props.selected)
+  return(
+    <div className='CircleSelector' >
+        <button 
+            className={props.selected === 'circleOne' ? 'selected' : ''} 
+            value='circleOne' 
+            onClick={handleChange}>{props.selected === 'circleOne' ? 'Circle 1 Selected' : 'Select Circle 1' }
+        </button>
+        <button 
+            className={props.selected === 'circleTwo' ? 'selected' : ''} 
+            value='circleTwo' 
+            onClick={handleChange}>{props.selected === 'circleTwo' ? 'Circle 2 Selected' : 'Select Circle 2' }
+        </button>
+        <button 
+            className={props.selected === 'circleThree' ? 'selected' : ''} 
+            value='circleThree' 
+            onClick={handleChange}>{props.selected === 'circleThree' ? 'Circle 3 Selected' : 'Select Circle 3' }
+        </button>
+        <button 
+            className={props.selected === 'circleFour' ? 'selected' : ''} 
+            value='circleFour' 
+            onClick={handleChange}>{props.selected === 'circle4' ? 'Circle 4 Selected' : 'Select Circle 4' }
+        </button>
+    </div>
+  )
+}
+
+export default CircleSelector;
